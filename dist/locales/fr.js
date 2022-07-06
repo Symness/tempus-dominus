81c3a9033097bf0ec059d57961b45eda6a7f0ed4
Simon <simon.frayssines@live.fr>
2022-07-06 14:33:00 +0200

    Create fr.js
    French translation

---
+++ dist/locales/fr.js
@@ -0,0 +1,37 @@
+/*!
+  * Tempus Dominus v6.0.0-beta8 (https://getdatepicker.com/)
+  * Copyright 2013-2022 Jonathan Peterson
+  * Licensed under MIT (https://github.com/Eonasdan/tempus-dominus/blob/master/LICENSE)
+  */
+(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f((g.tempusDominus=g.tempusDominus||{},g.tempusDominus.locales=g.tempusDominus.locales||{},g.tempusDominus.locales.fr={})));})(this,(function(exports){'use strict';const name = 'fr';
+const localization = {
+    today: 'Aujourd\'hui',
+    clear: 'Effacer la sélection',
+    close: 'Fermer le sélecteur',
+    selectMonth: 'Sélectionnez un mois',
+    previousMonth: 'Mois précédent',
+    nextMonth: 'Mois suivant',
+    selectYear: 'Sélectionnez une année',
+    previousYear: 'Année précédente',
+    nextYear: 'Année suivante',
+    selectDecade: 'Sélectionner une décennie',
+    previousDecade: 'Décennie précédente',
+    nextDecade: 'Décennie suivante',
+    previousCentury: 'Siècle précédent',
+    nextCentury: 'Siècle suivant',
+    pickHour: 'Choisir les heures',
+    incrementHour: 'Augmenter les heures',
+    decrementHour: 'Diminuer les heures',
+    pickMinute: 'Choisir les minutes',
+    incrementMinute: 'Augmenter les minutes',
+    decrementMinute: 'Diminuer les minutes',
+    pickSecond: 'Choisir les secondes',
+    incrementSecond: 'Augmenter les secondes',
+    decrementSecond: 'Diminuer les secondes',
+    toggleMeridiem: 'Changer AM/PM',
+    selectTime: 'Sélectionner l\'heure',
+    selectDate: 'Sélectionner la date',
+    dayViewHeaderFormat: { month: 'long', year: '4-digit' },
+    locale: 'fr',
+    startOfTheWeek: 1
+};exports.localization=localization;exports.name=name;Object.defineProperty(exports,'__esModule',{value:true});}));
